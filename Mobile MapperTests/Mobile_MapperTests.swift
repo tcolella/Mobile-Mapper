6c134747390ece81d6a93c2a1cf932349bd3e58c
tommy colella <tcolella@bsd220.org>
2020-02-24 10:16:38 -0600

    Initial Commit

---
+++ Mobile MapperTests/Mobile_MapperTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mobile_MapperTests.swift
+//  Mobile MapperTests
+//
+//  Created by Tommy Colella on 2/24/20.
+//  Copyright © 2020 Tommy Colella. All rights reserved.
+//
+
+import XCTest
+@testable import Mobile_Mapper
+
+class Mobile_MapperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
