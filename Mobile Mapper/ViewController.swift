ceb642cb29bb6eb1a962bfb917d3617bcb3500cb
tommy colella <tcolella@bsd220.org>
2020-02-24 10:31:41 -0600

    Added a location to the map

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -6,12 +6,20 @@
 //  Copyright © 2020 Tommy Colella. All rights reserved.
 //
 
-import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    let barringtonAnnotation = MKPointAnnotation()
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let latitude = 42.15704
+         let longitude = -88.14812
+         let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+         barringtonAnnotation.coordinate = coordinate
+         barringtonAnnotation.title = "Barrington High School"
+         mapView.addAnnotation(barringtonAnnotation)
         // Do any additional setup after loading the view.
     }
 
