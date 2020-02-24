1a077606f3a2cde7359452092dfb7c26e730117b
tommy colella <tcolella@bsd220.org>
2020-02-24 10:37:29 -0600

    Added Geocoder

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -12,22 +12,33 @@ import CoreLocation
 class ViewController: UIViewController {
     let barringtonAnnotation = MKPointAnnotation()
     let locationManager = CLLocationManager()
+    let address = "Mount Rushmore"
+    let geocoder = CLGeocoder()
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let latitude = 42.15704
-         let longitude = -88.14812
-         let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
-         barringtonAnnotation.coordinate = coordinate
-         barringtonAnnotation.title = "Barrington High School"
-         mapView.addAnnotation(barringtonAnnotation)
+        let longitude = -88.14812
+        let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        barringtonAnnotation.coordinate = coordinate
+        barringtonAnnotation.title = "Barrington High School"
+        mapView.addAnnotation(barringtonAnnotation)
         
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
+        
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+           for placemark in placemarks! {
+              let annotation = MKPointAnnotation()
+              annotation.coordinate = (placemark.location?.coordinate)!
+              annotation.title = placemark.name
+              self.mapView.addAnnotation(annotation)
+           }
+        }
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
